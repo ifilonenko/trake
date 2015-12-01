340c441035a2757256bb929c45e9830f5752c630
thomas wong <thomascwong1996@gmail.com>
2015-11-30 19:12:25 -0500

    changed methods to reflect change to Trail, fixed 80 char lines

--- test.ml
+++ test.ml
@@ -11,7 +11,17 @@ TEST_UNIT = Grid.status_of_cell grid (1, 2) === Grid.Wall
 TEST_UNIT = Grid.status_of_cell grid (2, 2) === Grid.Wall
 TEST_UNIT = Grid.status_of_cell grid (0, 1) === Grid.Wall
 TEST_UNIT = Grid.status_of_cell grid (2, 1) === Grid.Wall
-TEST_UNIT = Grid.status_of_cell grid (1, 1) === Grid.Empty                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+TEST_UNIT = Grid.status_of_cell grid (1, 1) === Grid.Empty
+
+let new_grid = Grid.create (20, 20)
+
+TEST_UNIT = Grid.dimensions new_grid === (20, 20)
+let added_players = Grid.add_player new_grid 
+
+TEST_UNIT = Player.vector_of_direction Player.Up === (0, 1)
+TEST_UNIT = Player.vector_of_direction Player.Down === (0, -1)
+TEST_UNIT = Player.vector_of_direction Player.Left === (-1, 0)
+TEST_UNIT = Player.vector_of_direction Player.Right === (1, 0)
 
 (* 
 TEST "status_of_cell_grid" = 
