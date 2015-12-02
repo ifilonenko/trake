953f25ad273ee974e55eb166e6df0017f37a16c4
Eric Morris <elm232@cornell.edu>
2015-12-02 16:28:31 -0500

    Update default grid size and add proposed protocols

--- trake.js
+++ trake.js
@@ -10,12 +10,12 @@ function getRandomColor() {
 TRAKE = {
 
   grid: {
-    height: 500,
-    width: 500,
-    rows: 20,
-    cols: 20,
-    border: 4,
-    color: "#cccccc"
+    height: 640,
+    width: 640,
+    rows: 100,
+    cols: 100,
+    border: 2,
+    color: "#eee"
   },
 
   setCell: function(x,y,color) {
@@ -43,6 +43,10 @@ TRAKE = {
     }
   },
 
+  updateGrid: function(gridJSON) {
+
+  },
+
   init: function() {
     this.ctx = document.querySelector('canvas').getContext('2d');
     this.makeGrid();
@@ -50,36 +54,124 @@ TRAKE = {
 
 }
 
-var socket = io('http://localhost:3110');
 
-// var WebSocketClient = require('websocket').client,
-//     client = new WebSocketClient();
 
-// client.on('connect', function(connection) {
-//   console.log('WebSocket Client Connected');
-//   connection.on('error', function(error) {
-//     console.log("Connection Error: " + error.toString());
-//   });
-//   connection.on('close', function() {
-//     console.log('echo-protocol Connection Closed');
-//   });
-//   connection.on('message', function(message) {
-//     TRAKE.game.color = getRandomColor();
-//     TRAKE.init();
+TRAKE.init();
 
-//     if (message.type === 'utf8') {
-//       console.log("Received: '" + message.utf8Data + "'");
-//     }
-//   });
 
-//   function sendNumber() {
-//     if (connection.connected) {
-//       var number = Math.round(Math.random() * 0xFFFFFF);
-//       connection.sendUTF(number.toString());
-//       setTimeout(sendNumber, 1000);
-//     }
-//   }
-//   sendNumber();
-// });
 
-// client.connect(SERVER_URL);
+var tick0 = {
+  type: "Initial",
+  walls: [
+    {x: 0, y: 0},
+    {x: 1, y: 0},
+    {x: 2, y: 0},
+    {x: 3, y: 0}
+  ],
+  food: {
+    x: 3,
+    y: 3
+  },
+  dimensions: {
+    rows: 100,
+    cols: 100
+  },
+  players: [
+    {
+      id: 0,
+      label: "Player 1",
+      color: "#b31b1b",
+      direction: "Up",
+      tail_length: 5,
+      score: 0,
+      position: {
+        x: 10,
+        y: 10
+      },
+      human: true
+    },
+    {
+      id: 1,
+      label: "Player 2 (AI)",
+      color: "#008800",
+      direction: "Down",
+      tail_length: 5,
+      score: 0,
+      position: {
+        x: 11,
+        y: 17
+      },
+      human: false
+    }
+  ]
+}
+
+var tick1 = {
+  type: "Update",
+  food: {
+    x: 3,
+    y: 3
+  },
+  players: [
+    {
+      id: 0,
+      direction: "Up",
+      tail_length: 5,
+      score: 2,
+      position: {
+        x: 10,
+        y: 11
+      }
+    },
+    {
+      id: 1,
+      direction: "Left",
+      tail_length: 5,
+      score: 0,
+      position: {
+        x: 11,
+        y: 18
+      }
+    }
+  ]
+}
+
+var tickN = {
+  type: "End",
+  final_scores: [
+    {id: 0, score: 200},
+    {id: 1, score: 50}
+  ]
+}
+
+
+// Join Protocol
+//
+//
+// Joining during waiting period
+// =====
+// Client: CONNECT
+// Server: ACCEPT CONNECTION
+// Client: {player_name: "Eric"}
+// Server: {id: 0}
+// [Pause Until Waiting Period Ends]
+// Server: Tick0
+// [Pause 5 Seconds]
+// Server: Tick1
+// Client: {id: 0, direction: "Up"}
+// Server: Tick2
+// ...
+
+// Joining during running game
+// =====
+// Client: CONNECT
+// Server: ACCEPT CONNECTION
+// Client: {player_name: "Eric"}
+// Server: {id: 0}
+// [Pause Until Current Game Ends]
+// Server: Tick0
+// [Pause 5 Seconds]
+// Server: Tick1
+// Client: {id: 0, direction: "Up"}
+// Server: Tick2
+// ...
