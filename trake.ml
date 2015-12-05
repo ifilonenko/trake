822074b2ac26c9936f4c3e610f3323624ab52cc0
Ilan Filonenko <ifilondz@gmail.com>
2015-12-05 16:08:02 -0500

    comment

--- trake.ml
+++ trake.ml
@@ -2,11 +2,12 @@
 (* sudo apt-get install -qq -yy libgmp-dev *)
 (* opam install "websocket=2.1" *)
 (* cs3110 compile -p yojson,lwt.unix,websocket.lwt,core -thread trake *)
+(* cs3110 run trake.ml *)
 
 let g = Grid.create (20,20)
 let s = Game.create "0.0.0.0" 3110 g
   Game.{ trail_length = 5;
-          ticks_per_second = 5.;
+          ticks_per_second = 3.;
           food_probability = 0.5;
           time_between_games = 5.;
           game_over_handler = fun hum ai -> (hum <= 1 && ai = 0) || (hum = 0 && ai <= 2)
