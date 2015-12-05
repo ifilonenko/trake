22b702b9145d61bad6fe8a02947dc4696e9993e3
Ilan Filonenko <ifilondz@gmail.com>
2015-12-05 01:57:27 -0500

    make game more interesting by adding more length, ticks, and probability

--- trake.ml
+++ trake.ml
@@ -4,6 +4,6 @@
 (* cs3110 compile -p yojson,lwt.unix,websocket.lwt,core -thread trake *)
 
 let g = Grid.create (20,20)
-let s = Game.create "0.0.0.0" 3110 g Game.{ trail_length = 5; ticks_per_second = 1.; food_probability = 0.2; }
+let s = Game.create "0.0.0.0" 3110 g Game.{ trail_length = 10; ticks_per_second = 5.; food_probability = 0.5; }
 let () = Server.serve "./" "0.0.0.0" 8080
 let _ = Lwt_main.run (Game.start s)
